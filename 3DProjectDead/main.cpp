2b14cbcb2eb3eb419b4e855095eb90c405a7c5ab
Niko <51508087+nurkert@users.noreply.github.com>
2021-01-17 13:53:53 +0100

    Initial Commit

---
+++ 3DProjectDead/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  3DProjectDead
+//
+//  Created by Niko Burkert on 17.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
